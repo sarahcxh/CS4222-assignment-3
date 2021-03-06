d8d40423badb6bc1cf26421451bb943e956f47a9
Sarah <e0035831@u.nus.edu>
2021-03-07 01:29:26 +0800

    Modified average calculation script

--- get_average.py
+++ get_average.py
@@ -1,12 +1,29 @@
-f = open("1m", "r")
-count = 0
-total = 0
+filenames = ['1m', '5m', '10m', '15m', '20m', '25m', '30m']
 
-for line in f:
-  fields = line.split(" ")
-  val = int(fields[3])
-  count += 1
-  total += val
+for f in filenames:
+    file = open(f, 'r')
+    packetCounter = 1
+    packets = []
 
-average = total/count
-print(average)
+    for i in range(1, 41):
+        packets.append(str(i))
+    missedPackets = []
+    receivedPackets = []
+    rssiReading = 0
+    total = 0
+
+    for line in file:
+        fields = line.split(" ")
+        receivedPackets.append(fields[1][:-1])
+        rssiReading += int(fields[3])
+        total += 1
+
+    for packet in packets:
+        if packet not in receivedPackets:
+            missedPackets.append(packet)
+
+    print("Finished reading file " + f + ".")
+    print("Packet loss:")
+    print(missedPackets)
+    print("Average RSSI: ")
+    print(rssiReading/total)
